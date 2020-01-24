c6d49f0c57a8ebf58e819239bdf92e57630aa5fd
patcantin <patcantin23@gmail.com>
2020-01-23 19:37:30 -0500

    longest word refined

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,33 +7,34 @@ class GamesController < ApplicationController
       keys = ""
       grid_size.times { keys += ('a'..'z').to_a.sample.to_s }
       @letters = keys.split(//)
-      $passing = keys.split(//)
-      $old = keys.split(//)
     end
     generate_grid(rand(4..12))
   end
 
   def score
-    #raise
+  # raise
+   @list = params[:letter_list].gsub(/\s+/, '')
+   @arr = @list.split("")
 
     url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
     user_serialized = open(url).read
     user = JSON.parse(user_serialized)
 
     arr = params[:answer].split(//)
+    word = params[:answer]
     arr.each do |x|
-      if $passing.include?(x)
-        $passing.delete_at($passing.index(x) || $passing.length)
+      if @arr.include?(x)
+        @arr.delete_at(@arr.index(x) || @arr.length)
           if user['found'] == true
-            @alert = "Congratulations! #{params[:answer].upcase} is a valid english word!"
+            @alert = "Congratulations! #{word.upcase} is a valid english word!"
             @score = "Your Score : #{params[:answer].length}"
           else
-            @alert = 'Sorry but ' + params[:answer].upcase + ' does not seem to be a valid english word.'
+            @alert = 'Sorry but ' + word.upcase + ' does not seem to be a valid english word.'
           end
       else
-        @alert = "Sorry but " + params[:answer].upcase + " can't be build of " + $old.join(",")
+        @alert = "Sorry but " + word.upcase + " can't be build of " + @arr
       end
-      @lett = "Letters left : #{$passing.join(",")}"
+      @lett = "Letters left : #{@arr.join(",")}"
     end
     # binding.pry
   end
